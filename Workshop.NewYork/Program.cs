6baf7788de3ee1162e786c6f81d24e5b4a1beb7f
Ayende Rahien <Ayende@ayende.com>
2018-02-12 06:13:09 -0800

    Initial commit

---
+++ Workshop.NewYork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Workshop.NewYork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
