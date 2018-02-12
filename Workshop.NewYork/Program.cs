d12fc468a4069349b80e955995bb6c822f1cdc31
Ayende Rahien <Ayende@ayende.com>
2018-02-12 09:21:53 -0800

    Simple operations

--- Workshop.NewYork/Program.cs
+++ Workshop.NewYork/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Documents.Queries;
+using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Workshop.NewYork
 {
@@ -6,7 +10,41 @@ namespace Workshop.NewYork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using(var store = new DocumentStore
+            {
+                Urls = new[]
+                {
+                    "https://a.newyork.dbs.local.ravendb.net"
+                },
+                Database = "nw",
+                Certificate = new X509Certificate2(@"C:\Users\ayende\Downloads\newyork.Cluster.Settings\admin.client.certificate.newyork.pfx")
+            })
+            {
+                store.Initialize();
+
+
+                var a = store.Operations.Send(new PatchByQueryOperation(new IndexQuery
+                {
+                    Query = @"
+from Employees 
+update {
+    var parts = this.FullName.split(' ');
+    this.FirstName = parts[0];
+    this.LastName = parts[1];
+    delete this.FullName;
+    this.User = $user;
+}
+",
+                    QueryParameters = new Raven.Client.Parameters
+                    {
+                        ["user"] = "users/2"
+                    }
+                })
+                {
+                    
+                });
+
+            }
         }
     }
 }
